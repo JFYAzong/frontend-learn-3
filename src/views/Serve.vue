9be3a778a99b903b1d95d4607f2a5bed97f134a2
JFYAzong <1018720708@qq.com>
2022-07-23 22:06:38 +0800

    Update Serve.vue

--- src/views/Serve.vue
+++ src/views/Serve.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>服务页面</div>
+  <div class="title-box">
+    <div class="title-box__title">XX服务中心 . 访客列表</div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.title-box {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 1017px;
+  background: linear-gradient(180deg, #7db7df 0%, #6aace7 100%);
+  &__title {
+    position: absolute;
+    top: 60px;
+    bottom: 60px;
+    left: 51px;
+    right: 1209px;
+    font-size: 45px;
+    font-family: PingFangSC-Medium, PingFang SC;
+    font-weight: 500;
+    color: rgba(255, 255, 255, 0.85);
+    line-height: 63px;
+    letter-spacing: 1px;
+  }
+}
 </style>
